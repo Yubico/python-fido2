71a83fbc82227c04ab64100fdb81c551aa9dd873
merlokk <807634+merlokk@users.noreply.github.com>
2019-05-25 12:19:39 -0700

    small fix

--- test/test_nfc.py
+++ test/test_nfc.py
@@ -33,8 +33,6 @@ import sys
 from fido2.hid import CTAPHID
 
 if True:
-    sys.modules['smartcard.Exceptions'] = mock.Mock()
-    sys.modules['smartcard.System'] = mock.Mock()
     sys.modules['smartcard'] = mock.Mock()
     from fido2.nfc import CtapNfcDevice
 
