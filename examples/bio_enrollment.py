f5fad8819fae15ccabc15f3349e936ed9cd7f821
Dain Nilsson <dain@yubico.com>
2020-02-10 15:25:59 +0100

    Add draft bio enrollment.

---
+++ examples/bio_enrollment.py
@@ -0,0 +1,75 @@
+# Copyright (c) 2020 Yubico AB
+# All rights reserved.
+#
+#   Redistribution and use in source and binary forms, with or
+#   without modification, are permitted provided that the following
+#   conditions are met:
+#
+#    1. Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#    2. Redistributions in binary form must reproduce the above
+#       copyright notice, this list of conditions and the following
+#       disclaimer in the documentation and/or other materials provided
+#       with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+"""
+Connects to the first FIDO device found over USB, and attempts to enroll a new
+fingerprint. This requires that a PIN is already set.
+
+NOTE: This uses a draft bio enrollment specification which is not yet final.
+Consider this highly experimental.
+"""
+from __future__ import print_function, absolute_import, unicode_literals
+
+from fido2.hid import CtapHidDevice
+from fido2.ctap2 import CTAP2, PinProtocolV1, FPBioEnrollment
+from getpass import getpass
+import sys
+
+pin = None
+uv = "discouraged"
+
+dev = next(CtapHidDevice.list_devices(), None)
+if not dev:
+    print("No FIDO device found")
+    sys.exit(1)
+
+ctap = CTAP2(dev)
+info = ctap.get_info()
+if not info.options.get("clientPin"):
+    print("PIN not set for the device!")
+    sys.exit(1)
+
+if "bioEnroll" not in info.options:
+    if "userVerificationMgmtPreview" not in info.options:  # TODO: Remove later
+        print("Device does not support bio enrollment!")
+        sys.exit(1)
+
+# Authenticate with PIN
+print("Preparing to enroll a new fingerprint.")
+pin = getpass("Please enter PIN: ")
+pin_token = PinProtocolV1(ctap).get_pin_token(pin)
+bio = FPBioEnrollment(ctap, PinProtocolV1.VERSION, pin_token)
+
+print(bio.enumerate_enrollments())
+
+# Start enrollment
+enroller = bio.enroll()
+template_id = None
+while template_id is None:
+    print("Press your fingerprint against the sensor now...")
+    template_id = enroller.capture()
+print("Fingerprint registered successfully with ID:", template_id)
--- fido2/ctap2.py
+++ fido2/ctap2.py
@@ -612,6 +612,8 @@ class CTAP2(object):
         CLIENT_PIN = 0x06
         RESET = 0x07
         GET_NEXT_ASSERTION = 0x08
+        # 0x40 is the command byte for bio enrollment preview
+        BIO_ENROLLMENT = 0x40  # 0x09
         # 0x41 is the command byte for credmgmt preview
         CREDENTIAL_MGMT = 0x41
 
@@ -807,14 +809,52 @@ class CTAP2(object):
         """CTAP2 credentialManagement command, used to manage resident
         credentials.
 
-        :param sub_cmd: A credentialManagement sub command.
+        NOTE: This implements the current draft version of the CTAP2 specification and
+        should be considered highly experimental.
+
+        This method is not intended to be called directly. It is intended to be used by
+        an instance of the CredentialManagement class.
+
+        :param sub_cmd: A CredentialManagement sub command.
         :param sub_cmd_params: Sub command specific parameters.
         :param pin_protocol: PIN protocol version used.
-        :pin_auth:
+        :param pin_auth: PIN auth param.
         """
         return self.send_cbor(
             CTAP2.CMD.CREDENTIAL_MGMT,
             args(sub_cmd, sub_cmd_params, pin_protocol, pin_auth),
+        )
+
+    def bio_enrollment(
+        self,
+        modality=None,
+        sub_cmd=None,
+        sub_cmd_params=None,
+        pin_protocol=None,
+        pin_auth=None,
+        get_modality=None,
+    ):
+        """CTAP2 bio enrollment command. Used to provision/enumerate/delete bio
+        enrollments in the authenticator.
+
+        NOTE: This implements the current draft version of the CTAP2 specification and
+        should be considered highly experimental.
+
+        This method is not intended to be called directly. It is intended to be used by
+        an instance of the BioEnrollment class.
+
+        :param modality: The user verification modality being used.
+        :param sub_cmd: A BioEnrollment sub command.
+        :param sub_cmd_params: Sub command specific parameters.
+        :param pin_protocol: PIN protocol version used.
+        :param pin_auth: PIN auth param.
+        :param get_modality: Get the user verification type modality.
+        """
+        return self.send_cbor(
+            CTAP2.CMD.BIO_ENROLLMENT,
+            args(
+                modality, sub_cmd, sub_cmd_params, pin_protocol, pin_auth, get_modality
+            ),
         )
 
     def get_assertions(self, *args, **kwargs):
@@ -855,17 +895,21 @@ class PinProtocolV1(object):
 
     @unique
     class CMD(IntEnum):
-        GET_RETRIES = 0x01
+        GET_PIN_RETRIES = 0x01
         GET_KEY_AGREEMENT = 0x02
         SET_PIN = 0x03
         CHANGE_PIN = 0x04
-        GET_PIN_TOKEN = 0x05
+        GET_UV_TOKEN_PIN = 0x05
+        GET_UV_TOKEN_UV = 0x06
+        GET_UV_RETRIES = 0x07
 
     @unique
     class RESULT(IntEnum):
         KEY_AGREEMENT = 0x01
-        PIN_TOKEN = 0x02
-        RETRIES = 0x03
+        UV_TOKEN = 0x02
+        PIN_RETRIES = 0x03
+        POWER_CYCLE_STATE = 0x04
+        UV_RETRIES = 0x05
 
     def __init__(self, ctap):
         self.ctap = ctap
@@ -897,10 +941,10 @@ class PinProtocolV1(object):
         return Cipher(algorithms.AES(secret), modes.CBC(PinProtocolV1.IV), be)
 
     def get_pin_token(self, pin):
-        """Get a PIN token from the authenticator.
+        """Get a UV token from the authenticator using PIN.
 
         :param pin: The PIN of the authenticator.
-        :return: A PIN token.
+        :return: A UV token.
         """
         key_agreement, shared_secret = self.get_shared_secret()
 
@@ -911,22 +955,57 @@ class PinProtocolV1(object):
 
         resp = self.ctap.client_pin(
             PinProtocolV1.VERSION,
-            PinProtocolV1.CMD.GET_PIN_TOKEN,
+            PinProtocolV1.CMD.GET_UV_TOKEN_PIN,
             key_agreement=key_agreement,
             pin_hash_enc=pin_hash_enc,
         )
         dec = cipher.decryptor()
-        return dec.update(resp[PinProtocolV1.RESULT.PIN_TOKEN]) + dec.finalize()
+        return dec.update(resp[PinProtocolV1.RESULT.UV_TOKEN]) + dec.finalize()
+
+    def get_uv_token(self):
+        """Get a UV token from the authenticator using built-in UV.
+
+        :return: A UV token.
+        """
+        key_agreement, shared_secret = self.get_shared_secret()
+
+        cipher = self._get_cipher(shared_secret)
+
+        resp = self.ctap.client_pin(
+            PinProtocolV1.VERSION,
+            PinProtocolV1.CMD.GET_UV_TOKEN_UV,
+            key_agreement=key_agreement,
+        )
+        dec = cipher.decryptor()
+        return dec.update(resp[PinProtocolV1.RESULT.UV_TOKEN]) + dec.finalize()
 
     def get_pin_retries(self):
         """Get the number of PIN retries remaining.
 
-        :return: The number or PIN attempts until the authenticator is locked.
+        :return: A tuple of the number of PIN attempts remaining until the
+        authenticator is locked, and the power cycle state, if available.
         """
         resp = self.ctap.client_pin(
-            PinProtocolV1.VERSION, PinProtocolV1.CMD.GET_RETRIES
+            PinProtocolV1.VERSION, PinProtocolV1.CMD.GET_PIN_RETRIES
         )
-        return resp[PinProtocolV1.RESULT.RETRIES]
+        return (
+            resp[PinProtocolV1.RESULT.PIN_RETRIES],
+            resp.get(PinProtocolV1.RESULT.POWER_CYCLE_STATE),
+        )
+
+    def get_uv_retries(self):
+        """Get the number of UV retries remaining.
+
+        :return: A tuple of the number of UV attempts remaining until the
+        authenticator is locked, and the power cycle state, if available.
+        """
+        resp = self.ctap.client_pin(
+            PinProtocolV1.VERSION, PinProtocolV1.CMD.GET_UV_RETRIES
+        )
+        return (
+            resp[PinProtocolV1.RESULT.UV_RETRIES],
+            resp.get(PinProtocolV1.RESULT.POWER_CYCLE_STATE),
+        )
 
     def set_pin(self, pin):
         """Set the PIN of the autenticator.
@@ -999,7 +1078,7 @@ class CredentialManagement(object):
         DELETE_CREDENTIAL = 0x06
 
     @unique
-    class SUB_PARAMETER(IntEnum):
+    class PARAM(IntEnum):
         RP_ID_HASH = 0x01
         CREDENTIAL_ID = 0x02
 
@@ -1087,7 +1166,7 @@ class CredentialManagement(object):
         """
         return self._call(
             CredentialManagement.CMD.ENUMERATE_CREDS_BEGIN,
-            {CredentialManagement.SUB_PARAMETER.RP_ID_HASH: rp_id_hash},
+            {CredentialManagement.PARAM.RP_ID_HASH: rp_id_hash},
         )
 
     def enumerate_creds_next(self):
@@ -1126,5 +1205,265 @@ class CredentialManagement(object):
         """
         return self._call(
             CredentialManagement.CMD.DELETE_CREDENTIAL,
-            {CredentialManagement.SUB_PARAMETER.CREDENTIAL_ID: cred_id},
+            {CredentialManagement.PARAM.CREDENTIAL_ID: cred_id},
+        )
+
+
+class BioEnrollment(object):
+    @unique
+    class RESULT(IntEnum):
+        MODALITY = 0x01
+        FINGERPRINT_KIND = 0x02
+        MAX_SAMPLES_REQUIRED = 0x03
+        TEMPLATE_ID = 0x04
+        LAST_SAMPLE_STATUS = 0x05
+        REMAINING_SAMPLES = 0x06
+        TEMPLATE_INFOS = 0x07
+
+    @unique
+    class TEMPLATE_INFO(IntEnum):
+        ID = 0x01
+        NAME = 0x02
+
+    @unique
+    class MODALITY(IntEnum):
+        FINGERPRINT = 0x01
+
+    def __init__(self, ctap, modality):
+        self.ctap = ctap
+        self.modality = self.get_modality()
+        if modality != self.modality:
+            raise ValueError("Device does not support {%s}".format(modality))
+
+    def get_modality(self):
+        """Get bio modality.
+
+        :return: The type of modality supported by the authenticator.
+        """
+        return self.ctap.bio_enrollment(get_modality=True)[
+            BioEnrollment.RESULT.MODALITY
+        ]
+
+
+class CaptureError(Exception):
+    def __init__(self, code):
+        self.code = code
+        super(CaptureError, self).__init__("Fingerprint capture error: %s" % code)
+
+
+class FPEnrollmentContext(object):
+    """Helper object to perform fingerprint enrollment.
+
+    :param bio: An instance of FPBioEnrollment.
+    :param timeout: Optional timeout for fingerprint captures (ms).
+    """
+
+    def __init__(self, bio, timeout=None):
+        self._bio = bio
+        self.timeout = timeout
+        self.template_id = None
+
+    def capture(self):
+        """Capture a fingerprint sample.
+
+        This call will block for up to timeout milliseconds (or indefinitely, if
+        timeout not specified) waiting for the user to scan their fingerprint to
+        collect one sample.
+
+        :return: None, if more samples are needed, or the template ID if enrollment is
+            completed.
+        """
+        if self.template_id is None:
+            self.template_id, status, remaining = self._bio.enroll_begin(self.timeout)
+        else:
+            status, remaining = self._bio.enroll_capture_next(
+                self.template_id, self.timeout
+            )
+        if status != FPBioEnrollment.FEEDBACK.FP_GOOD:
+            raise CaptureError(status)
+        if remaining == 0:
+            return self.template_id
+        return None
+
+    def cancel(self):
+        """Cancels ongoing enrollment."""
+        self._bio.enroll_cancel()
+        self.template_id = None
+
+
+class FPBioEnrollment(BioEnrollment):
+    """Implementation of a draft specification of the bio enrollment API.
+    WARNING: This specification is not final and this class is likely to change.
+
+    NOTE: The get_modality and get_fingerprint_sensor_info methods do not require
+    authentication, and can be used by setting modality, pin_protocol, and pin_token to
+    None.
+
+    :param ctap: An instance of a CTAP2 object.
+    :param modality: The modality to use, only FINGERPRINT is currently supported.
+    :param pin_protocol: The PIN protocol version used.
+    :param pin_token: A valid pin_token for the current CTAP session.
+    """
+
+    @unique
+    class CMD(IntEnum):
+        ENROLL_BEGIN = 0x01
+        ENROLL_CAPTURE_NEXT = 0x02
+        ENROLL_CANCEL = 0x03
+        ENUMERATE_ENROLLMENTS = 0x04
+        SET_NAME = 0x05
+        REMOVE_ENROLLMENT = 0x06
+        GET_SENSOR_INFO = 0x07
+
+    @unique
+    class PARAM(IntEnum):
+        TEMPLATE_ID = 0x01
+        TEMPLATE_NAME = 0x02
+        TIMEOUT_MS = 0x03
+
+    @unique
+    class FEEDBACK(IntEnum):
+        FP_GOOD = 0x00
+        FP_TOO_HIGH = 0x01
+        FP_TOO_LOW = 0x02
+        FP_TOO_LEFT = 0x03
+        FP_TOO_RIGHT = 0x04
+        FP_TOO_FAST = 0x05
+        FP_TOO_SLOW = 0x06
+        FP_POOR_QUALITY = 0x07
+        FP_TOO_SKEWED = 0x08
+        FP_TOO_SHORT = 0x09
+        FP_MERGE_FAILURE = 0x0A
+        FP_EXISTS = 0x0B
+        FP_DATABASE_FULL = 0x0C
+        NO_USER_ACTIVITY = 0x0D
+        NO_UP_TRANSITION = 0x0E
+
+        def __str__(self):
+            return "0x%02X - %s" % (self.value, self.name)
+
+    def __init__(self, ctap, pin_protocol, pin_token):
+        super(FPBioEnrollment, self).__init__(ctap, BioEnrollment.MODALITY.FINGERPRINT)
+        self.pin_protocol = pin_protocol
+        self.pin_token = pin_token
+
+    def _call(self, sub_cmd, params=None, auth=True):
+        if params is not None:
+            params = {k: v for k, v in params.items() if v is not None}
+        kwargs = {
+            "modality": self.modality,
+            "sub_cmd": sub_cmd,
+            "sub_cmd_params": params,
+        }
+        if auth:
+            msg = struct.pack(">BB", self.modality, sub_cmd)
+            if params is not None:
+                msg += cbor.encode(params)
+            kwargs["pin_protocol"] = self.pin_protocol
+            kwargs["pin_auth"] = hmac_sha256(self.pin_token, msg)[:16]
+        return self.ctap.bio_enrollment(**kwargs)
+
+    def get_fingerprint_sensor_info(self):
+        """Get fingerprint sensor info.
+
+        :return: A dict containing FINGERPRINT_KIND and MAX_SAMPLES_REQUIRES.
+        """
+        return self._call(FPBioEnrollment.CMD.GET_SENSOR_INFO, auth=False)
+
+    def enroll_begin(self, timeout=None):
+        """Start fingerprint enrollment.
+
+        Starts the process of enrolling a new fingerprint, and will wait for the user
+        to scan their fingerprint once to provide an initial sample.
+
+        :param timeout: Optional timeout in milliseconds.
+        :return: A tuple containing the new template ID, the sample status, and the
+            number of samples remaining to complete the enrollment.
+        """
+        result = self._call(
+            FPBioEnrollment.CMD.ENROLL_BEGIN,
+            {FPBioEnrollment.PARAM.TIMEOUT_MS: timeout},
+        )
+        return (
+            result[BioEnrollment.RESULT.TEMPLATE_ID],
+            FPBioEnrollment.FEEDBACK(result[BioEnrollment.RESULT.LAST_SAMPLE_STATUS]),
+            result[BioEnrollment.RESULT.REMAINING_SAMPLES],
+        )
+
+    def enroll_capture_next(self, template_id, timeout=None):
+        """Continue fingerprint enrollment.
+
+        Continues enrolling a new fingerprint and will wait for the user to scan their
+        fingerpring once to provide a new sample.
+        Once the number of samples remaining is 0, the enrollment is completed.
+
+        :param template_id: The template ID returned by a call to `enroll_begin`.
+        :param timeout: Optional timeout in milliseconds.
+        :return: A tuple containing the sample status, and the number of samples
+            remaining to complete the enrollment.
+        """
+        result = self._call(
+            FPBioEnrollment.CMD.ENROLL_CAPTURE_NEXT,
+            {
+                FPBioEnrollment.PARAM.TEMPLATE_ID: template_id,
+                FPBioEnrollment.PARAM.TIMEOUT_MS: timeout,
+            },
+        )
+        return (
+            FPBioEnrollment.FEEDBACK(result[BioEnrollment.RESULT.LAST_SAMPLE_STATUS]),
+            result[BioEnrollment.RESULT.REMAINING_SAMPLES],
+        )
+
+    def enroll_cancel(self):
+        """Cancel any ongoing fingerprint enrollment."""
+        self._call(FPBioEnrollment.CMD.ENROLL_CANCEL, auth=False)
+
+    def enroll(self, timeout=None):
+        """Convenience wrapper for doing fingerprint enrollment.
+
+        See FPEnrollmentContext for details.
+        :return: An initialized FPEnrollmentContext.
+        """
+        return FPEnrollmentContext(self, timeout)
+
+    def enumerate_enrollments(self):
+        """Get a dict of enrolled fingerprint templates which maps template ID's to
+        their friendly names.
+
+        :return: A dict of enrolled template_id -> name pairs.
+        """
+        try:
+            return {
+                t[BioEnrollment.TEMPLATE_INFO.ID]: t[BioEnrollment.TEMPLATE_INFO.NAME]
+                for t in self._call(FPBioEnrollment.CMD.ENUMERATE_ENROLLMENTS)[
+                    BioEnrollment.RESULT.TEMPLATE_INFOS
+                ]
+            }
+        except CtapError as e:
+            if e.code == CtapError.ERR.INVALID_OPTION:
+                return []
+            raise
+
+    def set_name(self, template_id, name):
+        """Set/Change the friendly name of a previously enrolled fingerprint template.
+
+        :param template_id: The ID of the template to change.
+        :param name: A friendly name to give the template.
+        """
+        self._call(
+            FPBioEnrollment.CMD.SET_NAME,
+            {
+                BioEnrollment.TEMPLATE_INFO.ID: template_id,
+                BioEnrollment.TEMPLATE_INFO.NAME: name,
+            },
+        )
+
+    def remove_enrollment(self, template_id):
+        """Remove a previously enrolled fingerprint template.
+
+        :param template_id: The Id of the template to remove.
+        """
+        self._call(
+            FPBioEnrollment.CMD.REMOVE_ENROLLMENT,
+            {BioEnrollment.TEMPLATE_INFO.ID: template_id},
         )
