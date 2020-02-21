b1182fac0347fe54629b5c21e53f62932d674e7f
Dain Nilsson <dain@yubico.com>
2020-02-21 09:31:52 +0100

    Replace use of "python setup.py test".
    This command has been deprecated from setuptools. Instead we now:
    - Specify test dependencies as development dependencies in Pipfile.
    - Explicitly invoke the unittest test runner, via pipenv.

--- setup.py
+++ setup.py
@@ -59,8 +59,6 @@ setup(
         'uhid-freebsd>=1.2.1;platform_system=="FreeBSD"',
     ],
     extras_require={':python_version < "3.4"': ["enum34"], "pcsc": ["pyscard"]},
-    test_suite="test",
-    tests_require=["mock<4", 'pyfakefs>=3.4;platform_system=="Linux"'],
     classifiers=[
         "License :: OSI Approved :: BSD License",
         "License :: OSI Approved :: Apache Software License",
