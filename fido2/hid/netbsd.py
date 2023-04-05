fc46e83f7c3f92bcec4c7f39aab06d510eaf88d0
Dain Nilsson <dain@yubico.com>
2023-04-05 11:48:07 +0200

    Fix pre-commit issues.

--- fido2/hid/netbsd.py
+++ fido2/hid/netbsd.py
@@ -21,20 +21,25 @@ import logging
 import os
 import select
 import struct
+import sys
 
-from ctypes import Structure
-from ctypes import c_char
-from ctypes import c_int
-from ctypes import c_ubyte
-from ctypes import c_uint16
-from ctypes import c_uint32
-from ctypes import c_uint8
-from ctypes import sizeof
-from fcntl import ioctl
+from ctypes import (
+    Structure,
+    c_char,
+    c_int,
+    c_ubyte,
+    c_uint16,
+    c_uint32,
+    c_uint8,
+)
 from typing import Set
 
 from . import base
 
+# Don't typecheck this file on Windows
+assert sys.platform != "win32"  # nosec
+
+from fcntl import ioctl  # noqa: E402
 
 logger = logging.getLogger(__name__)
 
@@ -47,40 +52,40 @@ USB_MAX_ENCODED_STRING_LEN = USB_MAX_STRING_LEN * 3
 
 class usb_ctl_report_desc(Structure):
     _fields_ = [
-        ('ucrd_size', c_int),
-        ('ucrd_data', c_ubyte * 1024),
+        ("ucrd_size", c_int),
+        ("ucrd_data", c_ubyte * 1024),
     ]
 
 
 class usb_device_info(Structure):
     _fields_ = [
-        ('udi_bus', c_uint8),
-        ('udi_addr', c_uint8),
-        ('udi_pad0', c_uint8 * 2),
-        ('udi_cookie', c_uint32),
-        ('udi_product', c_char * USB_MAX_ENCODED_STRING_LEN),
-        ('udi_vendor', c_char * USB_MAX_ENCODED_STRING_LEN),
-        ('udi_release', c_char * 8),
-        ('udi_serial', c_char * USB_MAX_ENCODED_STRING_LEN),
-        ('udi_productNo', c_uint16),
-        ('udi_vendorNo', c_uint16),
-        ('udi_releaseNo', c_uint16),
-        ('udi_class', c_uint8),
-        ('udi_subclass', c_uint8),
-        ('udi_protocol', c_uint8),
-        ('udi_config', c_uint8),
-        ('udi_speed', c_uint8),
-        ('udi_pad1', c_uint8),
-        ('udi_power', c_int),
-        ('udi_nports', c_int),
-        ('udi_devnames', c_char * USB_MAX_DEVNAMES * USB_MAX_DEVNAMELEN),
-        ('udi_ports', c_uint8 * 16),
+        ("udi_bus", c_uint8),
+        ("udi_addr", c_uint8),
+        ("udi_pad0", c_uint8 * 2),
+        ("udi_cookie", c_uint32),
+        ("udi_product", c_char * USB_MAX_ENCODED_STRING_LEN),
+        ("udi_vendor", c_char * USB_MAX_ENCODED_STRING_LEN),
+        ("udi_release", c_char * 8),
+        ("udi_serial", c_char * USB_MAX_ENCODED_STRING_LEN),
+        ("udi_productNo", c_uint16),
+        ("udi_vendorNo", c_uint16),
+        ("udi_releaseNo", c_uint16),
+        ("udi_class", c_uint8),
+        ("udi_subclass", c_uint8),
+        ("udi_protocol", c_uint8),
+        ("udi_config", c_uint8),
+        ("udi_speed", c_uint8),
+        ("udi_pad1", c_uint8),
+        ("udi_power", c_int),
+        ("udi_nports", c_int),
+        ("udi_devnames", c_char * USB_MAX_DEVNAMES * USB_MAX_DEVNAMELEN),
+        ("udi_ports", c_uint8 * 16),
     ]
 
 
-USB_GET_DEVICE_INFO = 0x44f45570  # _IOR('U', 112, struct usb_device_info)
+USB_GET_DEVICE_INFO = 0x44F45570  # _IOR('U', 112, struct usb_device_info)
 USB_GET_REPORT_DESC = 0x44045515  # _IOR('U', 21, struct usb_ctl_report_desc)
-USB_HID_SET_RAW = 0x80046802      # _IOW('h', 2, int)
+USB_HID_SET_RAW = 0x80046802  # _IOW('h', 2, int)
 
 
 # Cache for continuously failing devices
@@ -93,7 +98,7 @@ def list_descriptors():
     descriptors = []
 
     for i in range(100):
-        path = '/dev/uhid%d' % (i,)
+        path = "/dev/uhid%d" % (i,)
         stale.discard(path)
         try:
             desc = get_descriptor(path)
@@ -101,14 +106,12 @@ def list_descriptors():
             if e.errno == errno.ENOENT:
                 break
             if path not in _failed_cache:
-                logger.debug("Failed opening FIDO device %s", path,
-                    exc_info=True)
+                logger.debug("Failed opening FIDO device %s", path, exc_info=True)
                 _failed_cache.add(path)
             continue
         except Exception:
             if path not in _failed_cache:
-                logger.debug("Failed opening FIDO device %s", path,
-                    exc_info=True)
+                logger.debug("Failed opening FIDO device %s", path, exc_info=True)
                 _failed_cache.add(path)
             continue
         descriptors.append(desc)
@@ -120,21 +123,21 @@ def list_descriptors():
 def get_descriptor(path):
     fd = None
     try:
-        fd = os.open(path, os.O_RDONLY|os.O_CLOEXEC)
+        fd = os.open(path, os.O_RDONLY | os.O_CLOEXEC)
         devinfo = usb_device_info()
         ioctl(fd, USB_GET_DEVICE_INFO, devinfo)
         ucrd = usb_ctl_report_desc()
         ioctl(fd, USB_GET_REPORT_DESC, ucrd)
-        report_desc = bytearray(ucrd.ucrd_data[:ucrd.ucrd_size])
+        report_desc = bytearray(ucrd.ucrd_data[: ucrd.ucrd_size])
         maxin, maxout = base.parse_report_descriptor(report_desc)
         vid = devinfo.udi_vendorNo
         pid = devinfo.udi_productNo
         try:
-            name = devinfo.udi_product.decode('utf-8')
+            name = devinfo.udi_product.decode("utf-8")
         except UnicodeDecodeError:
             name = None
         try:
-            serial = devinfo.udi_serial.decode('utf-8')
+            serial = devinfo.udi_serial.decode("utf-8")
         except UnicodeDecodeError:
             serial = None
         return base.HidDescriptor(path, vid, pid, maxin, maxout, name, serial)
@@ -144,7 +147,7 @@ def get_descriptor(path):
 
 
 def open_connection(descriptor):
-    return NetBSDCtapHidConnection(descriptor);
+    return NetBSDCtapHidConnection(descriptor)
 
 
 class NetBSDCtapHidConnection(base.FileCtapHidConnection):
@@ -153,9 +156,9 @@ class NetBSDCtapHidConnection(base.FileCtapHidConnection):
         # closed
         super().__init__(descriptor)
         try:
-            ioctl(self.handle, USB_HID_SET_RAW, struct.pack('@i', 1))
+            ioctl(self.handle, USB_HID_SET_RAW, struct.pack("@i", 1))
             ping = bytearray(64)
-            ping[0:7] = bytearray([0xff,0xff,0xff,0xff,0x81,0,1])
+            ping[0:7] = bytearray([0xFF, 0xFF, 0xFF, 0xFF, 0x81, 0, 1])
             for i in range(10):
                 self.write_packet(ping)
                 poll = select.poll()
@@ -164,7 +167,7 @@ class NetBSDCtapHidConnection(base.FileCtapHidConnection):
                     self.read_packet()
                     break
             else:
-                raise Exception('u2f ping timeout')
+                raise Exception("u2f ping timeout")
         except Exception:
             self.close()
             raise
