368921af8ff4ca8f2c922be8781c669714134f6f
Dain Nilsson <dain@yubico.com>
2025-01-08 13:07:51 +0100

    Fix get_version in docs

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ def get_version():
     with open("../pyproject.toml", "rb") as f:
         pyproject = tomllib.load(f)
 
-    return pyproject["tool"]["poetry"]["version"]
+    return pyproject["project"]["version"]
 
 
 # -- Project information -----------------------------------------------------
