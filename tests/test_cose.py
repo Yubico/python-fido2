9e160151cb16047b9aadfc0b73c79da77b68e194
Emil Lundberg <emil@yubico.com>
2025-04-14 15:12:00 +0200

    Add fully-specified COSE algorithm identifiers
    As specified in:
    https://datatracker.ietf.org/doc/draft-ietf-jose-fully-specified-algorithms/

--- tests/test_cose.py
+++ tests/test_cose.py
@@ -29,7 +29,7 @@
 from __future__ import absolute_import, unicode_literals
 
 from fido2 import cbor
-from fido2.cose import CoseKey, ES256, RS256, EdDSA, UnsupportedKey
+from fido2.cose import CoseKey, ES256, ESP256, RS256, Ed25519, EdDSA, UnsupportedKey
 from binascii import a2b_hex
 
 import unittest
@@ -38,12 +38,14 @@ import unittest
 _ES256_KEY = a2b_hex(
     b"A5010203262001215820A5FD5CE1B1C458C530A54FA61B31BF6B04BE8B97AFDE54DD8CBB69275A8A1BE1225820FA3A3231DD9DEED9D1897BE5A6228C59501E4BCD12975D3DFF730F01278EA61C"  # noqa E501
 )
+_ESP256_KEY = _ES256_KEY[0:4] + a2b_hex("28") + _ES256_KEY[5:]
 _RS256_KEY = a2b_hex(
     b"A401030339010020590100B610DCE84B65029FAE24F7BF8A1730D37BC91435642A628E691E9B030BF3F7CEC59FF91CBE82C54DE16C136FA4FA8A58939B5A950B32E03073592FEC8D8B33601C04F70E5E2D5CF7B4E805E1990EA5A86928A1B390EB9026527933ACC03E6E41DC0BE40AA5EB7B9B460743E4DD80895A758FB3F3F794E5E9B8310D3A60C28F2410D95CF6E732749A243A30475267628B456DE770BC2185BBED1D451ECB0062A3D132C0E4D842E0DDF93A444A3EE33A85C2E913156361713155F1F1DC64E8E68ED176466553BBDE669EB82810B104CB4407D32AE6316C3BD6F382EC3AE2C5FD49304986D64D92ED11C25B6C5CF1287233545A987E9A3E169F99790603DBA5C8AD2143010001"  # noqa E501
 )
 _EdDSA_KEY = a2b_hex(
     b"a4010103272006215820ee9b21803405d3cf45601e58b6f4c06ea93862de87d3af903c5870a5016e86f5"  # noqa E501
 )
+_Ed25519_KEY = _EdDSA_KEY[0:4] + a2b_hex("3831") + _EdDSA_KEY[5:]
 
 
 class TestCoseKey(unittest.TestCase):
@@ -55,6 +57,33 @@ class TestCoseKey(unittest.TestCase):
             {
                 1: 2,
                 3: -7,
+                -1: 1,
+                -2: a2b_hex(
+                    b"A5FD5CE1B1C458C530A54FA61B31BF6B04BE8B97AFDE54DD8CBB69275A8A1BE1"
+                ),
+                -3: a2b_hex(
+                    b"FA3A3231DD9DEED9D1897BE5A6228C59501E4BCD12975D3DFF730F01278EA61C"
+                ),
+            },
+        )
+        key.verify(
+            a2b_hex(
+                b"0021F5FC0B85CD22E60623BCD7D1CA48948909249B4776EB515154E57B66AE12010000002C"  # noqa E501
+                + b"7B89F12A9088B0F5EE0EF8F6718BCCC374249C31AEEBAEB79BD0450132CD536C"
+            ),
+            a2b_hex(
+                b"304402202B3933FE954A2D29DE691901EB732535393D4859AAA80D58B08741598109516D0220236FBE6B52326C0A6B1CFDC6BF0A35BDA92A6C2E41E40C3A1643428D820941E0"  # noqa E501
+            ),
+        )
+
+    def test_ESP256_parse_verify(self):
+        key = CoseKey.parse(cbor.decode(_ESP256_KEY))
+        self.assertIsInstance(key, ESP256)
+        self.assertEqual(
+            key,
+            {
+                1: 2,
+                3: -9,
                 -1: 1,
                 -2: a2b_hex(
                     b"A5FD5CE1B1C458C530A54FA61B31BF6B04BE8B97AFDE54DD8CBB69275A8A1BE1"
@@ -106,6 +135,29 @@ class TestCoseKey(unittest.TestCase):
             {
                 1: 1,
                 3: -8,
+                -1: 6,
+                -2: a2b_hex(
+                    "EE9B21803405D3CF45601E58B6F4C06EA93862DE87D3AF903C5870A5016E86F5"
+                ),
+            },
+        )
+        key.verify(
+            a2b_hex(
+                b"a379a6f6eeafb9a55e378c118034e2751e682fab9f2d30ab13d2125586ce1947010000000500a11a323057d1103784ddff99a354ddd42348c2f00e88d8977b916cabf92268"  # noqa E501
+            ),
+            a2b_hex(
+                b"e8c927ef1a57c738ff4ba8d6f90e06d837a5219eee47991f96b126b0685d512520c9c2eedebe4b88ff2de2b19cb5f8686efc7c4261e9ed1cb3ac5de50869be0a"  # noqa E501
+            ),
+        )
+
+    def test_Ed25519_parse_verify(self):
+        key = CoseKey.parse(cbor.decode(_Ed25519_KEY))
+        self.assertIsInstance(key, Ed25519)
+        self.assertEqual(
+            key,
+            {
+                1: 1,
+                3: -50,
                 -1: 6,
                 -2: a2b_hex(
                     "EE9B21803405D3CF45601E58B6F4C06EA93862DE87D3AF903C5870A5016E86F5"
