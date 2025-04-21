1aec398d9471b9528083a295580ebfd76fd761e2
Dain Nilsson <dain@yubico.com>
2025-04-21 14:05:43 +0200

    Update ARKG for spec changes, add tests

--- fido2/arkg.py
+++ fido2/arkg.py
@@ -27,21 +27,26 @@
 
 import struct
 from dataclasses import dataclass
-from typing import Sequence, Tuple
+from typing import Sequence, Tuple, cast
 
 from cryptography.hazmat.primitives.hashes import SHA256, Hash, HashAlgorithm
 from cryptography.hazmat.primitives.hmac import HMAC
 from cryptography.hazmat.primitives.kdf.hkdf import HKDF
-from ecdsa import SigningKey
 from ecdsa.curves import Curve, NIST256p
 from ecdsa.ellipticcurve import Point
 
-from .cose import ES256, CoseKey
-from .utils import bytes2int, int2bytes
+from .cose import ESP256, CoseKey
+from .utils import bytes2int
 
 
 def strxor(a: bytes, b: bytes) -> bytes:
     return bytes(a[i] ^ b[i] for i in range(len(a)))
+
+
+def ecdh(point: Point, scalar: int) -> bytes:
+    enc = bytes((point * scalar).to_bytes("uncompressed"))
+    ln = len(enc) // 2
+    return enc[1 : ln + 1]
 
 
 @dataclass
@@ -166,12 +171,12 @@ class BL:
     Hash: HashAlgorithm
     DST_ext: bytes
 
-    def blind_public_key(self, pk: Point, tau: bytes, info: bytes) -> Point:
+    def blind_public_key(self, pk: Point, tau: bytes, ctx: bytes) -> Point:
         """
-        BL-Blind-Public-Key(pk, tau, info) -> pk_tau
+        BL-Blind-Public-Key(pk, tau, ctx) -> pk_tau
 
             tau' = hash_to_field(tau, 1) with the parameters:
-                DST: 'ARKG-BL-EC.' || DST_ext || info
+                DST: 'ARKG-BL-EC.' || DST_ext || ctx
                 F: GF(N), the scalar field
                    of the prime order subgroup of crv
                 p: N
@@ -182,7 +187,7 @@ class BL:
 
             pk_tau = pk + tau' * G
         """
-        dst = b"ARKG-BL-EC." + self.DST_ext + info
+        dst = b"ARKG-BL-EC." + self.DST_ext + ctx
         htf = HTF(dst, self.crv.order, 48, self.Hash())
 
         tau_prime = htf.hash_to_field(tau, 1)[0]
@@ -197,16 +202,29 @@ class KEM:
     Hash: HashAlgorithm
     DST_ext: bytes
 
-    def sub_kem_generate(self) -> Tuple[Point, int]:
+    def sub_kem_derive_key_pair(self, ikm) -> Tuple[Point, int]:
         """
-        Sub-Kem-Generate-Keypair() -> (pk, sk)
+        Sub-Kem-Derive-Key-Pair(ikm) -> (pk, sk)
 
-            Generate (pk, sk) using some procedure defined for crv.
+            sk = hash_to_field(ikm, 1) with the parameters:
+                DST: 'ARKG-KEM-ECDH-KG.' || DST_ext
+                F: GF(N), the scalar field
+                  of the prime order subgroup of crv
+                p: N
+                m: 1
+                L: The L defined in hash-to-crv-suite
+                expand_message: The expand_message function
+                                defined in hash-to-crv-suite
+
+            pk = sk * G
         """
-        sk = SigningKey.generate(NIST256p)
-        return sk.verifying_key.pubkey.point, sk.privkey.secret_multiplier
+        htf = HTF(b"ARKG-KEM-ECDH-KG." + self.DST_ext, self.crv.order, 48, self.Hash())
+        sk = htf.hash_to_field(ikm, 1)[0]
+        pk: Point = sk * cast(Point, self.crv.generator)
 
-    def sub_kem_encaps(self, pk: Point, info: bytes) -> Tuple[bytes, bytes]:
+        return pk, sk
+
+    def sub_kem_encaps(self, pk: Point, ikm: bytes, ctx: bytes) -> Tuple[bytes, bytes]:
         """
         ECDH(pk, sk) represents the compact output of ECDH [RFC6090]
         using public key (curve point) pk and private key (exponent) sk.
@@ -215,26 +233,25 @@ class KEM:
 
         N is the order of G.
 
-        Sub-Kem-Encaps(pk, info) -> (k, c)
+        Sub-Kem-Encaps(pk, ikm, ctx) -> (k, c)
 
-            (pk', sk') = Sub-Kem-Generate-Keypair()
+            (pk', sk') = Sub-Kem-Derive-Key-Pair(ikm)
 
             k = ECDH(pk, sk')
             c = Elliptic-Curve-Point-to-Octet-String(pk')
         """
-        pk_prime, sk_prime = self.sub_kem_generate()
-        # TODO: Don't hardcode length
-        k = int2bytes((pk * sk_prime).x(), 32)
+        pk_prime, sk_prime = self.sub_kem_derive_key_pair(ikm)
+        k = ecdh(pk, sk_prime)
         c = bytes(pk_prime.to_bytes("uncompressed"))
 
         return k, c
 
-    def encaps(self, pk: Point, info: bytes) -> Tuple[bytes, bytes]:
+    def encaps(self, pk: Point, ikm: bytes, ctx: bytes) -> Tuple[bytes, bytes]:
         """
-        KEM-Encaps(pk, info) -> (k, c)
+        KEM-Encaps(pk, ikm, ctx) -> (k, c)
 
-            info_sub = 'ARKG-KEM-HMAC.' || DST_ext || info
-            (k', c') = Sub-Kem-Encaps(pk, info_sub)
+            ctx_sub = 'ARKG-KEM-HMAC.' || DST_ext || ctx
+            (k', c') = Sub-Kem-Encaps(pk, ikm, ctx_sub)
 
             prk = HKDF-Extract with the arguments:
                 Hash: Hash
@@ -244,29 +261,28 @@ class KEM:
             mk = HKDF-Expand with the arguments:
                 Hash: Hash
                 PRK: prk
-                info: 'ARKG-KEM-HMAC-mac.' || DST_ext || info
+                info: 'ARKG-KEM-HMAC-mac.' || DST_ext || ctx
                 L: L
             t = HMAC-Hash-128(K=mk, text=c')
 
             k = HKDF-Expand with the arguments:
                 Hash: Hash
                 PRK: prk
-                info: 'ARKG-KEM-HMAC-shared.' || DST_ext || info
+                info: 'ARKG-KEM-HMAC-shared.' || DST_ext || ctx
                 L: The length of k' in octets.
             c = t || c'
         """
 
-        dst_ext = self.DST_ext
         h = self.Hash()
 
-        info_sub = b"ARKG-KEM-HMAC." + dst_ext + info
-        k_prime, c_prime = self.sub_kem_encaps(pk, info_sub)
+        ctx_sub = b"ARKG-KEM-HMAC." + self.DST_ext + ctx
+        k_prime, c_prime = self.sub_kem_encaps(pk, ikm, ctx_sub)
 
         mk = HKDF(
             h,
             h.digest_size,
             None,
-            b"ARKG-KEM-HMAC-mac." + dst_ext + info,
+            b"ARKG-KEM-HMAC-mac." + self.DST_ext + ctx,
         ).derive(k_prime)
 
         hmac = HMAC(mk, h)
@@ -277,7 +293,7 @@ class KEM:
             h,
             len(k_prime),
             None,
-            b"ARKG-KEM-HMAC-shared." + dst_ext + info,
+            b"ARKG-KEM-HMAC-shared." + self.DST_ext + ctx,
         ).derive(k_prime)
 
         c = t + c_prime
@@ -297,15 +313,21 @@ class ARKG:
     kem: KEM
 
     def derive_public_key(
-        self, pk_kem: Point, pk_bl: Point, info: bytes
+        self, pk_bl: Point, pk_kem: Point, ikm: bytes, ctx: bytes
     ) -> Tuple[Point, bytes]:
         """
-        ARKG-Derive-Public-Key((pk_kem, pk_bl), info) -> (pk', kh)
+        ARKG-Derive-Public-Key((pk_bl, pk_kem), ikm, ctx) -> (pk', kh)
+            ARKG instance parameters:
+                BL        A key blinding scheme.
+                KEM       A key encapsulation mechanism.
+
             Inputs:
-                pk_kem    A key encapsulation public key.
                 pk_bl     A key blinding public key.
-                info      An octet string containing optional context
-                            and application specific information
+                pk_kem    A key encapsulation public key.
+                ikm       Input entropy for KEM encapsulation.
+                ctx       An octet string of length at most 64,
+                            containing optional context and
+                            application specific information
                             (can be a zero-length string).
 
             Output:
@@ -315,97 +337,112 @@ class ARKG:
 
             The output (pk', kh) is calculated as follows:
 
-            info_kem = 'ARKG-Derive-Key-KEM.' || info
-            info_bl  = 'ARKG-Derive-Key-BL.'  || info
+            if LEN(ctx) > 64:
+                Abort with an error.
 
-            (tau, c) = KEM-Encaps(pk_kem, info_kem)
-            pk' = BL-Blind-Public-Key(pk_bl, tau, info_bl)
+            ctx'    = I2OSP(LEN(ctx), 1) || ctx
+            ctx_bl  = 'ARKG-Derive-Key-BL.'  || ctx'
+            ctx_kem = 'ARKG-Derive-Key-KEM.' || ctx'
+
+            (tau, c) = KEM-Encaps(pk_kem, ikm, ctx_kem)
+            pk' = BL-Blind-Public-Key(pk_bl, tau, ctx_bl)
 
             kh = c
         """
-        info_kem = b"ARKG-Derive-Key-KEM." + info
-        info_bl = b"ARKG-Derive-Key-BL." + info
+        assert len(ctx) <= 64  # noqa: S101
 
-        tau, c = self.kem.encaps(pk_kem, info_kem)
-        pk_prime = self.bl.blind_public_key(pk_bl, tau, info_bl)
+        ctx_prime = struct.pack(">B", len(ctx)) + ctx
+        ctx_bl = b"ARKG-Derive-Key-BL." + ctx_prime
+        ctx_kem = b"ARKG-Derive-Key-KEM." + ctx_prime
+
+        tau, c = self.kem.encaps(pk_kem, ikm, ctx_kem)
+        pk_prime = self.bl.blind_public_key(pk_bl, tau, ctx_bl)
 
         kh = c
 
         return pk_prime, kh
 
 
-"""
-The identifier ARKG-P256ADD-ECDH represents the following ARKG instance:
-
-    BL: Elliptic curve addition as described in Section 3.1 with the parameters:
-
-        crv: The NIST curve secp256r1 [SEC2].
-
-        hash-to-crv-suite: P256_XMD:SHA-256_SSWU_RO_ [RFC9380].
-
-        DST_ext: 'ARKG-P256ADD-ECDH'.
-
-    KEM: ECDH as described in Section 3.3 with the parameters:
-
-        crv: The NIST curve secp256r1 [SEC2].
-
-        Hash: SHA-256 [FIPS 180-4].
-
-        DST_ext: 'ARKG-P256ADD-ECDH'.
-"""
-
-
 def _cose2point(cose):
     return Point(NIST256p.curve, bytes2int(cose[-2]), bytes2int(cose[-3]))
 
 
-class ARKG_P256_DERIVED(ES256):
-    def __init__(self, *args, parent_kid: bytes, kh: bytes, info: bytes, **kwargs):
+ARKG_P256_ESP256 = -65539
+
+
+class ARKG_P256_DERIVED(ESP256):
+    def __init__(self, *args, parent_kid: bytes, kh: bytes, ctx: bytes, **kwargs):
         super().__init__(*args, **kwargs)
         self._parent_kid = parent_kid
         self._kh = kh
-        self._info = info
+        self._ctx = ctx
 
     def get_ref(self):
         return {
             1: -65538,  # kty: Ref-ARKG-derived
             2: self._parent_kid,
-            3: ARKG_P256ADD_ECDH.ALGORITHM,
+            3: ARKG_P256_ESP256,  # alg: ESP256 with key derived by ARKG-P256
             -1: self._kh,
-            -2: self._info,
+            -2: self._ctx,
+            -3: ARKG_P256.ALGORITHM,
         }
 
 
-class ARKG_P256ADD_ECDH(CoseKey):
-    ALGORITHM = -65539
+class ARKG_P256(CoseKey):
+    """
+    The identifier ARKG-P256 represents the following ARKG instance:
+
+        BL: Elliptic curve addition as described in Section 3.1 with the parameters:
+
+            crv: The NIST curve secp256r1 [SEC2].
+
+            hash-to-crv-suite: P256_XMD:SHA-256_SSWU_RO_ [RFC9380].
+
+            DST_ext: 'ARKG-P256'.
+
+        KEM: ECDH as described in Section 3.3 with the parameters:
+
+            crv: The NIST curve secp256r1 [SEC2].
+
+            Hash: SHA-256 [FIPS 180-4].
+
+            hash-to-crv-suite: P256_XMD:SHA-256_SSWU_RO_ [RFC9380].
+
+            DST_ext: 'ARKG-P256'.
+    """
+
+    ALGORITHM = -65700
     _ARKG = ARKG(
-        bl=BL(crv=NIST256p, Hash=SHA256, DST_ext=b"ARKG-P256ADD-ECDH"),
-        kem=KEM(crv=NIST256p, Hash=SHA256, DST_ext=b"ARKG-P256ADD-ECDH"),
+        bl=BL(crv=NIST256p, Hash=SHA256, DST_ext=b"ARKG-P256"),
+        kem=KEM(crv=NIST256p, Hash=SHA256, DST_ext=b"ARKG-ECDH.ARKG-P256"),
     )
 
     @property
-    def blinding_key(self) -> CoseKey:
+    def pkbl(self) -> CoseKey:
         return CoseKey.parse(self[-1])
 
     @property
-    def kem_key(self) -> CoseKey:
+    def pkkem(self) -> CoseKey:
         return CoseKey.parse(self[-2])
 
-    def derive_public_key(self, info: bytes) -> CoseKey:
+    def derive_public_key(self, ikm: bytes, ctx: bytes) -> CoseKey:
         point, kh = self._ARKG.derive_public_key(
-            _cose2point(self.kem_key),
-            _cose2point(self.blinding_key),
-            info,
+            _cose2point(self.pkbl),
+            _cose2point(self.pkkem),
+            ikm,
+            ctx,
         )
+        point_enc = point.to_bytes("uncompressed")
+        ln = len(point_enc) // 2
         return ARKG_P256_DERIVED(
             {
-                1: 2,
-                3: -7,
+                1: 2,  # kty: EC
+                3: self[-3],  # derived key alg (-9)
                 -1: 1,
-                -2: int2bytes(point.x(), 32),
-                -3: int2bytes(point.y(), 32),
+                -2: point_enc[1 : ln + 1],  # x-coordinate
+                -3: point_enc[1 + ln :],  # y-coordinate
             },
             parent_kid=self[2],
             kh=kh,
-            info=info,
+            ctx=ctx,
         )
