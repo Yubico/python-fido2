27e09990721f341200b98308116848d26b1c1375
Dain Nilsson <dain@yubico.com>
2026-06-29 18:53:35 +0200

    Fix examples for WindowsClient

--- examples/resident_key.py
+++ examples/resident_key.py
@@ -41,7 +41,7 @@ client, info = get_client(lambda info: info.options.get("rk"))
 
 # Prefer UV if supported and configured
 uv = "discouraged"
-if info and info.options.get("uv") or info.options.get("bioEnroll"):
+if info and (info.options.get("uv") or info.options.get("bioEnroll")):
     uv = "preferred"
     print("Authenticator is configured for User Verification")
 
